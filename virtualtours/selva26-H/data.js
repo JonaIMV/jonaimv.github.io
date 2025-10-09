88abcfb79b4d635972ee51539f34f7b1360e2f3c
JonaIMV <jonaimv@gmail.com>
2025-10-09 12:29:15 -0500

    virtualtour selva

---
+++ virtualtours/selva26-H/data.js
@@ -0,0 +1,621 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.405891897804759,
+        "pitch": 0.3267897287895085,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10166882414353218,
+          "pitch": 0.019515125791446764,
+          "rotation": 1.5707963267948966,
+          "target": "6-vista-aerea-"
+        },
+        {
+          "yaw": -1.4247181485417162,
+          "pitch": 0.39068433149512494,
+          "rotation": 0,
+          "target": "1-comedor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8656187925605954,
+          "pitch": 0.22879154859811024,
+          "title": "Cuarto de lavado&nbsp;",
+          "text": "Lavasecadora electrica"
+        },
+        {
+          "yaw": 0.5062233931653957,
+          "pitch": -0.3667195469318951,
+          "title": "Cocina integral",
+          "text": "Amplio espacio, cuenta con campana de estufa y horno de microhondas"
+        },
+        {
+          "yaw": 1.242712363894782,
+          "pitch": 0.36109072751810345,
+          "title": "Barra de granito",
+          "text": "Equipada con estufa, licuadora y cafetera"
+        }
+      ]
+    },
+    {
+      "id": "1-comedor",
+      "name": "Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.926888377694394,
+          "pitch": 0.3429736506418344,
+          "rotation": 0,
+          "target": "0-cocina"
+        },
+        {
+          "yaw": -1.0908734919949516,
+          "pitch": 0.28704025934775323,
+          "rotation": 0,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.044289987941397,
+          "pitch": 0.36565918255185714,
+          "title": "Comedor y barra",
+          "text": "&nbsp;4 sillas y barra para alimentos"
+        }
+      ]
+    },
+    {
+      "id": "2-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.73159962861771,
+          "pitch": 0.1944384050381096,
+          "rotation": 0,
+          "target": "4-bao"
+        },
+        {
+          "yaw": 1.4383835009642052,
+          "pitch": 0.033359217357578075,
+          "rotation": 17.27875959474387,
+          "target": "3-habitacin-1"
+        },
+        {
+          "yaw": 1.9182258597826456,
+          "pitch": 0.01806724297976281,
+          "rotation": 1.5707963267948966,
+          "target": "5-habitacin-2"
+        },
+        {
+          "yaw": -0.042314936073456266,
+          "pitch": 0.2051718591251479,
+          "rotation": 0,
+          "target": "6-vista-aerea-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.899421112342454,
+          "pitch": -0.36382478162652987,
+          "title": "Cortinas",
+          "text": "Cortinas black out"
+        },
+        {
+          "yaw": -2.020759988310747,
+          "pitch": 0.08141291763330116,
+          "title": "Sala",
+          "text": "Cómoda sala para pasar un agradable rato"
+        }
+      ]
+    },
+    {
+      "id": "3-habitacin-1",
+      "name": "Habitación 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.733973917484903,
+          "pitch": 0.14215851878864072,
+          "rotation": 4.71238898038469,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.835202365451412,
+          "pitch": -0.408910176628968,
+          "title": "Aire acondicionado",
+          "text": "<br>"
+        },
+        {
+          "yaw": 3.1331349598042113,
+          "pitch": -0.314260315094387,
+          "title": "Closet",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.1490423972125328,
+          "pitch": 0.16377523548629647,
+          "title": "Cama matrimonial",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-bao",
+      "name": "Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.581214305044144,
+          "pitch": 0.2257002860808086,
+          "rotation": 0,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5308107188133704,
+          "pitch": -0.04890910858386732,
+          "title": "Cristal templado",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-habitacin-2",
+      "name": "Habitación 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.857408988596692,
+          "pitch": 0.051888077874799876,
+          "rotation": 1.5707963267948966,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.764552509929942,
+          "pitch": -0.2393220908462812,
+          "title": "Closet",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.21068193442796357,
+          "pitch": 0.1425900825089439,
+          "title": "Cama Queen size",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9191825065122075,
+          "pitch": -0.8143202754236079,
+          "title": "Aire acondicionado",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-vista-aerea-",
+      "name": "Vista aerea ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.06791445377985461,
+        "pitch": 0.886003769947461,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.021210010650852595,
+          "pitch": 0.8669949144791609,
+          "rotation": 0,
+          "target": "0-cocina"
+        },
+        {
+          "yaw": -3.02558713973157,
+          "pitch": 0.34179550379362666,
+          "rotation": 0,
+          "target": "7-cancha-"
+        },
+        {
+          "yaw": 2.9447406090172237,
+          "pitch": 0.32956790013845705,
+          "rotation": 0,
+          "target": "8-gimnasio-aire-libre"
+        },
+        {
+          "yaw": -2.7316928121923443,
+          "pitch": 0.3381336745025436,
+          "rotation": 0,
+          "target": "9-alberca"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.12422183079430127,
+          "pitch": 0.8365917867923116,
+          "title": "26 H&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0624485698898454,
+          "pitch": 0.15331988278876452,
+          "title": "Amenidades",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-cancha-",
+      "name": "Cancha ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11840951255534193,
+          "pitch": -0.22312803485778687,
+          "rotation": 0,
+          "target": "6-vista-aerea-"
+        },
+        {
+          "yaw": 2.7113774979581455,
+          "pitch": 0.04054594615224261,
+          "rotation": 0,
+          "target": "8-gimnasio-aire-libre"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8617354327447124,
+          "pitch": -0.09768543267481178,
+          "title": "Cancha de usos multiples",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-gimnasio-aire-libre",
+      "name": "Gimnasio aire libre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6431369919135577,
+          "pitch": 0.022191546045945643,
+          "rotation": 0,
+          "target": "9-alberca"
+        },
+        {
+          "yaw": 1.725007156681312,
+          "pitch": 0.0017848729906351224,
+          "rotation": 0,
+          "target": "7-cancha-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5992309886732645,
+          "pitch": -0.22090620461198895,
+          "title": "Juegos infantiles",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-alberca",
+      "name": "Alberca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7381887807046557,
+          "pitch": 0.05306016966769889,
+          "rotation": 0,
+          "target": "8-gimnasio-aire-libre"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.746799544712573,
+          "pitch": -0.18639325212976132,
+          "title": "Palapa",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0592721551496034,
+          "pitch": -0.01626353463523955,
+          "title": "Camastros y asadores",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4308651342922722,
+          "pitch": 0.3509688498451222,
+          "title": "Alberca familiar",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
