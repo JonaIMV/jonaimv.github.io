c32934714568e4a3f68e4b2ed8a588be796131f4
JonaIMV <jonaimv@gmail.com>
2025-10-17 18:21:01 -0500

    tours virtuales agregados, departamento naay agregado

---
+++ virtualtours/naay-34-B/data.js
@@ -0,0 +1,571 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-departamento-34-b",
+      "name": "Departamento 34 B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 0.6545604996233259,
+        "pitch": -0.0894395712409004,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5943281242964353,
+          "pitch": -0.01662933842843728,
+          "rotation": 0,
+          "target": "7-estacionamiento"
+        },
+        {
+          "yaw": 0.8036016581525534,
+          "pitch": 0.12559378545887334,
+          "rotation": 0,
+          "target": "1-sala-comedor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5514798854734266,
+          "pitch": 0.13265700896586807,
+          "title": "Rack para bicicletas",
+          "text": "Se utiliza por los vecinos que tengan bicis"
+        },
+        {
+          "yaw": -2.2477746717686014,
+          "pitch": -0.023490434020056128,
+          "title": "Jardineria",
+          "text": "La administracion le da mantenimiento a toda la jardineria"
+        }
+      ]
+    },
+    {
+      "id": "1-sala-comedor",
+      "name": "Sala Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.0839480340421197,
+        "pitch": 0.5453528661379607,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.659144802520033,
+          "pitch": 0.06377324936781648,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": -3.1274676569677062,
+          "pitch": 0.05771690692388631,
+          "rotation": 0,
+          "target": "3-bao-1"
+        },
+        {
+          "yaw": 2.9239993829252686,
+          "pitch": 0.08180157340118832,
+          "rotation": 4.71238898038469,
+          "target": "4-habitacion-2"
+        },
+        {
+          "yaw": -2.7473577479928792,
+          "pitch": 0.08962705358897693,
+          "rotation": 1.5707963267948966,
+          "target": "5-habitacion-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1640169325662226,
+          "pitch": 0.4043841280486049,
+          "title": "Comedor",
+          "text": "Comedor para 4 personas con mesa de cristal&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.574538366005246,
+        "pitch": 0.2924588459681754,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1301982028954018,
+          "pitch": 0.1871515993439985,
+          "rotation": 0,
+          "target": "1-sala-comedor"
+        },
+        {
+          "yaw": 0.3925226440741252,
+          "pitch": 0.09619307553593437,
+          "rotation": 0,
+          "target": "0-departamento-34-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.797280649464609,
+          "pitch": -0.2812438449728578,
+          "title": "Cocina integral",
+          "text": "Estilo italiana, con parrilla electrica"
+        },
+        {
+          "yaw": 2.878508644393544,
+          "pitch": 0.003669301454745977,
+          "title": "Area de servicio",
+          "text": "Usado tambien como bodega&nbsp;"
+        },
+        {
+          "yaw": 0.6318464477361196,
+          "pitch": -0.1100790412544761,
+          "title": "Refrigerador 24 Pies",
+          "text": "Bastante espacio para todas las compras"
+        }
+      ]
+    },
+    {
+      "id": "3-bao-1",
+      "name": "Baño 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 0.18710514518522992,
+        "pitch": 0.6380792648436078,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8243169382133342,
+          "pitch": 0.15771174942049626,
+          "rotation": 0,
+          "target": "1-sala-comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-habitacion-2",
+      "name": "Habitacion 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15802513068217827,
+          "pitch": 0.07976447842453638,
+          "rotation": 4.71238898038469,
+          "target": "1-sala-comedor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.253165708717237,
+          "pitch": 0.43525132320712956,
+          "title": "Cama Canguro",
+          "text": "Cama Queen size con cama matrimonial abajo"
+        },
+        {
+          "yaw": 2.0273420842802654,
+          "pitch": -0.38843717149251233,
+          "title": "Aire Acondicionado",
+          "text": "12,000 BTU´s"
+        }
+      ]
+    },
+    {
+      "id": "5-habitacion-1",
+      "name": "Habitacion 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.5269273974439663,
+        "pitch": 0.6381613682669691,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1332653525291114,
+          "pitch": 0.011282196411073642,
+          "rotation": 0,
+          "target": "1-sala-comedor"
+        },
+        {
+          "yaw": 1.2067033406201162,
+          "pitch": 0.18818125731892366,
+          "rotation": 1.5707963267948966,
+          "target": "6-bao-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5261365453253024,
+          "pitch": 0.22297071378001654,
+          "title": "Camas Individuales",
+          "text": "Suficientes camas para todos"
+        },
+        {
+          "yaw": 2.248989320114287,
+          "pitch": -0.37619991555589394,
+          "title": "Aire acondicionado",
+          "text": "Pantalla de 42\"&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "6-bao-2",
+      "name": "Baño 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.3323035436392647,
+        "pitch": 0.49618194280942163,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0928935428822264,
+          "pitch": -0.05528849908308153,
+          "rotation": 1.5707963267948966,
+          "target": "1-sala-comedor"
+        },
+        {
+          "yaw": -0.4566243611966545,
+          "pitch": -0.0568961931210783,
+          "rotation": 4.71238898038469,
+          "target": "5-habitacion-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-estacionamiento",
+      "name": "Estacionamiento",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.7240527437250783,
+        "pitch": 0.1182788351445403,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0118214303090323,
+          "pitch": 0.1125611125816679,
+          "rotation": 0,
+          "target": "0-departamento-34-b"
+        },
+        {
+          "yaw": -1.839602125562216,
+          "pitch": 0.0467521032383722,
+          "rotation": 0,
+          "target": "8-alberca-y-asador"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.543220611452525,
+          "pitch": 0.06745008435610877,
+          "title": "Estacionamiento",
+          "text": "34-B"
+        }
+      ]
+    },
+    {
+      "id": "8-alberca-y-asador",
+      "name": "Alberca y asador",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.357896382434136,
+        "pitch": 0.3382708251733195,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9519826321733511,
+          "pitch": 0.05185506817588248,
+          "rotation": 0,
+          "target": "7-estacionamiento"
+        },
+        {
+          "yaw": -0.6600074434886167,
+          "pitch": -0.09920594279762796,
+          "rotation": 0,
+          "target": "9-gym"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4359086459376655,
+          "pitch": -0.0793867215121633,
+          "title": "Asador",
+          "text": "Sillas y camastros en esta area, lista para invitar a tus amigos"
+        }
+      ]
+    },
+    {
+      "id": "9-gym",
+      "name": "Gym",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.211835487962265,
+        "pitch": 0.20443903714120282,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7614751311190773,
+          "pitch": 0.24648616261898404,
+          "rotation": 0,
+          "target": "8-alberca-y-asador"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5215248985193046,
+          "pitch": 0.06023209827928433,
+          "title": "Gimnasio",
+          "text": "Cuenta con mancuernas, bicis estaticas, costal de box, y bancos, lo suficiente para no perder la forma."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
