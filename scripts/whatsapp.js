d0bb219f078b2654425d033b96ee0d80065f4f56
JonaIMV <jonaimv@gmail.com>
2025-12-29 13:47:09 -0500

    agregamos ads aw

--- scripts/whatsapp.js
+++ scripts/whatsapp.js
@@ -14,6 +14,7 @@ export function initWhatsApp() {
     waButton.onclick = function() {
         if (typeof fbq !== 'undefined') {
             fbq('track', 'Contact'); 
+        
         }
     };
    
