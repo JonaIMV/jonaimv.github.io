e1d0507949eee8c0c35c4fa2fec4409f8f34d02b
JonaIMV <jonaimv@gmail.com>
2026-01-03 09:43:47 -0500

    Agregado tour virtual y optimizaciones

---
+++ virtualtours/aldea-kiin/project-title/data.js
@@ -0,0 +1,684 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "Fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.2458287946479558,
+        "pitch": -0.3674367289523488,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3024087640901278,
+          "pitch": -0.06004794642595357,
+          "rotation": 0,
+          "target": "1-cocina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5137768201556057,
+          "pitch": -0.7052486222613918,
+          "title": "Fachada",
+          "text": "Piedra blanca, aluminio negro y ceramica para el balcón"
+        },
+        {
+          "yaw": -1.7120369477787793,
+          "pitch": 0.06168933696839396,
+          "title": "Respeto de la naturaleza en el frente&nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.9982855061013964,
+          "pitch": 0.013326787894957448,
+          "title": "Calle cerrada",
+          "text": "Sin paso de autos, la alberca queda en esa dirección."
+        }
+      ]
+    },
+    {
+      "id": "1-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.02558874705443,
+        "pitch": 0.13285196416168965,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2543969801150023,
+          "pitch": 0.10919657758694079,
+          "rotation": 0,
+          "target": "2-sala-comedor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1293047088393706,
+          "pitch": 0.31648110882374425,
+          "title": "Tarja",
+          "text": "Doble tarja de acero inoxidable&nbsp;"
+        },
+        {
+          "yaw": 2.2374384837068853,
+          "pitch": 0.3740586478697292,
+          "title": "Cocina integral",
+          "text": "Incluira refrigerador, estufa y campana&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-sala-comedor",
+      "name": "Sala-comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.2668504882943594,
+        "pitch": -0.02438391625127423,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5996677061737596,
+          "pitch": 0.034322842402524145,
+          "rotation": 0,
+          "target": "0-fachada"
+        },
+        {
+          "yaw": 1.2281377763787713,
+          "pitch": 0.06337993248038742,
+          "rotation": 0,
+          "target": "1-cocina"
+        },
+        {
+          "yaw": 1.7360806000991085,
+          "pitch": 0.08748947725060674,
+          "rotation": 0.7853981633974483,
+          "target": "3-pasillo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5140937822567766,
+          "pitch": -0.3421450115131446,
+          "title": "Doble altura",
+          "text": "Ideal para mantener el ambiente fresco"
+        },
+        {
+          "yaw": 2.066540884084458,
+          "pitch": 0.0233937009407974,
+          "title": "Escaleras",
+          "text": "Piedra blanca en todo el cubo de escaleras desde la planta baja hasta el ultimo nivel de la casa"
+        },
+        {
+          "yaw": -1.922285877487397,
+          "pitch": 0.2955438890359243,
+          "title": "Alberca",
+          "text": "Próximamente en el tour virtual&nbsp;"
+        },
+        {
+          "yaw": -0.01900722937295285,
+          "pitch": -0.06172381024832596,
+          "title": "Amplio espacio",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.8554305344855537,
+          "pitch": -0.08917855032323097,
+          "title": "Baño",
+          "text": "Próximamente en el tour virtual"
+        }
+      ]
+    },
+    {
+      "id": "3-pasillo",
+      "name": "Pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.266983907240462,
+        "pitch": 0.1603980920677266,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9405412695277686,
+          "pitch": 0.14616932722765696,
+          "rotation": 0,
+          "target": "6-habitacin-2"
+        },
+        {
+          "yaw": -1.5306885140752104,
+          "pitch": 0.135926868065944,
+          "rotation": 0,
+          "target": "4-habitacin-principal"
+        },
+        {
+          "yaw": 3.0140885390645673,
+          "pitch": 0.5215360354199845,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-comedor"
+        },
+        {
+          "yaw": 1.8708190729532967,
+          "pitch": 0.204857558256478,
+          "rotation": 0.7853981633974483,
+          "target": "8-pasillo-2do-nivel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-habitacin-principal",
+      "name": "Habitación principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.45210015477185195,
+        "pitch": 0.3383289235229121,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2068702586879105,
+          "pitch": 0.21063001130159087,
+          "rotation": 0,
+          "target": "3-pasillo"
+        },
+        {
+          "yaw": 2.9481025091733777,
+          "pitch": 0.20612387208457505,
+          "rotation": 0,
+          "target": "5-bao-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3700811954612124,
+          "pitch": 0.2840867239114253,
+          "title": "Balcón&nbsp;",
+          "text": "La entrada de aire esta garantizada sin la entrada de moscos gracias a los mosquiteros de cada ventanal&nbsp;"
+        },
+        {
+          "yaw": 1.5944305902474545,
+          "pitch": -0.05541671664601466,
+          "title": "Walking closet",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-bao-principal",
+      "name": "Baño principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.1158261578363486,
+        "pitch": 0.368670533615985,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1090086080229895,
+          "pitch": 0.1131831362630642,
+          "rotation": 0,
+          "target": "4-habitacin-principal"
+        },
+        {
+          "yaw": 2.324513395097968,
+          "pitch": 0.12384571761445073,
+          "rotation": 1.5707963267948966,
+          "target": "3-pasillo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6093822149079529,
+          "pitch": -0.12645670502568684,
+          "title": "Cristal templado",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-habitacin-2",
+      "name": "Habitación 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.4010317143782505,
+        "pitch": 0.1319452436099322,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.521546887510757,
+          "pitch": 0.1125571480376859,
+          "rotation": 0,
+          "target": "7-bao-2"
+        },
+        {
+          "yaw": 2.679155945637177,
+          "pitch": 0.1395141976902341,
+          "rotation": 0,
+          "target": "3-pasillo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bao-2",
+      "name": "Baño 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6340070402124294,
+          "pitch": 0.6070630349171999,
+          "rotation": 0,
+          "target": "6-habitacin-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pasillo-2do-nivel",
+      "name": "Pasillo 2do nivel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.6747714134055798,
+        "pitch": 0.2674502611877898,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1018985214775316,
+          "pitch": 0.38779839432095287,
+          "rotation": 0,
+          "target": "11-terraza"
+        },
+        {
+          "yaw": 2.8791519918532344,
+          "pitch": 0.4235293935908011,
+          "rotation": 0,
+          "target": "3-pasillo"
+        },
+        {
+          "yaw": -1.3523839164870886,
+          "pitch": 0.12747428425123886,
+          "rotation": 0,
+          "target": "9-habitacin-3"
+        },
+        {
+          "yaw": -0.6747714134055798,
+          "pitch": 0.2674502611877898,
+          "rotation": 0,
+          "target": "10-habitacin-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7290039868337317,
+          "pitch": -0.06695686785235822,
+          "title": "Baño 3&nbsp;",
+          "text": "Próximamente en el tour"
+        },
+        {
+          "yaw": 1.7882491037514843,
+          "pitch": -0.03590924823602393,
+          "title": "Área de lavanderia&nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "9-habitacin-3",
+      "name": "Habitación 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.7581868864619947,
+        "pitch": 0.24675156972321055,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3348154635162937,
+          "pitch": 0.1577916574537852,
+          "rotation": 0,
+          "target": "8-pasillo-2do-nivel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-habitacin-4",
+      "name": "Habitación 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.0203658910037134,
+        "pitch": 0.35347063061945505,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2854046868415399,
+          "pitch": 0.11932322848509358,
+          "rotation": 0,
+          "target": "8-pasillo-2do-nivel"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5368103076798167,
+          "pitch": -0.04533763090942067,
+          "title": "Habitación o área de tv",
+          "text": "Este espacio puede ser adaptado segun tus necesidades, area de tv gimnasio,estudio o una cuarta habitación"
+        }
+      ]
+    },
+    {
+      "id": "11-terraza",
+      "name": "Terraza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.6744219232066957,
+        "pitch": 0.22462915800721817,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1105104812233613,
+          "pitch": 0.24165469521540928,
+          "rotation": 0,
+          "target": "8-pasillo-2do-nivel"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
