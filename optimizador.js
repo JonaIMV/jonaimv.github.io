333723cd33bebab832681b43db4250742fbbf215
JonaIMV <jonaimv@gmail.com>
2026-07-04 13:40:09 -0500

    Nueva propiedad

--- optimizador.js
+++ optimizador.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp'); // <- Agregamos el compresor
 
-const carpetaPropiedad = 'strella'; // Cambia esto por el nombre de tu subcarpeta dentro de /images
-const nombreSEO = 'Departamento en VENTA Puerto Morelos Strella Mexico '; // Cambia esto por el nombre SEO que quieres usar en los ALT y nombres de archivo
+const carpetaPropiedad = 'palma-real-ii'; // Cambia esto por el nombre de tu subcarpeta dentro de /images
+const nombreSEO = 'Casa en Venta Puerto Morelos Quintana Roo'; // Cambia esto por el nombre SEO que quieres usar en los ALT y nombres de archivo
 
 // Rutas basadas en la estructura
 const rutaImagenes = path.join('./images', carpetaPropiedad);
